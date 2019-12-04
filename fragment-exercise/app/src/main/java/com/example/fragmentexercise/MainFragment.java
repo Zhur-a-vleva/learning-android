a3c428256984fc07e0ac3149388a10b1ebcf39d9
Zhur-a-vleva <Zhur.a.vleva.Darya21@gmail.com>
2019-12-04 09:50:10 +0300

    Added: button which call dialog, button which close dialog

--- fragment-exercise/app/src/main/java/com/example/fragmentexercise/MainFragment.java
+++ fragment-exercise/app/src/main/java/com/example/fragmentexercise/MainFragment.java
@@ -1,5 +1,8 @@
 package com.example.fragmentexercise;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -26,6 +30,7 @@ public class MainFragment extends Fragment {
     return view;
   }
 
+  //Added: onClickListener for dialog_button
   @Override
   public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
     Button button = view.findViewById(R.id.click);
@@ -43,6 +48,29 @@ public class MainFragment extends Fragment {
           default:
             break;
         }
+      }
+    });
+    Button show_dialog = view.findViewById(R.id.dialog_button);
+    show_dialog.setOnClickListener(new OnClickListener() {
+      @SuppressLint("ResourceType")
+      @RequiresApi(api = VERSION_CODES.LOLLIPOP)
+      @Override
+      public void onClick(View v) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        final AlertDialog dialog = builder.create();
+
+        View dialog_view = getLayoutInflater().inflate(R.layout.dialog, null);
+        dialog.setView(dialog_view);
+
+        Button exit = dialog_view.findViewById(R.id.button_exit);
+        exit.setOnClickListener(new OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            dialog.cancel();
+          }
+        });
+
+        dialog.show();
       }
     });
     super.onViewCreated(view, savedInstanceState);
