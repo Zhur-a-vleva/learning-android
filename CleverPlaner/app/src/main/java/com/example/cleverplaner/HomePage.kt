30f74f864f58cc4b6aeaa2950ed37b23337cc078
Stanislav Zemlyakov <redtom@yandex.ru>
2020-02-13 17:38:30 +0300

    Add todo, fixme

--- CleverPlaner/app/src/main/java/com/example/cleverplaner/HomePage.kt
+++ CleverPlaner/app/src/main/java/com/example/cleverplaner/HomePage.kt
@@ -25,21 +25,26 @@ class HomePage : AppCompatActivity() {
 
         //setting navigation on bottomNavigation
         bottomNavigationView.setOnNavigationItemSelectedListener {
+            // FIXME: переделать на when(it.id) { }
             if (it.itemId == R.id.bottom_navigation_list) {
                 fragmentTransaction
                     .add(R.id.fragment_container, List.newInstance())
                     .commit()
                 fragmentTransaction.addToBackStack(List.className)
+                true
             } else if (it.itemId == R.id.bottom_navigation_tasks) {
                 fragmentTransaction
                     .add(R.id.fragment_container, Tasks.newInstance())
                     .commit()
                 fragmentTransaction.addToBackStack(Tasks.className)
+                // TODO: поэкспериминтировать, что будет, если `false`
+                false
             } else {
                 fragmentTransaction
                     .add(R.id.fragment_container, Settings.newInstance())
                     .commit()
                 fragmentTransaction.addToBackStack(Settings.className)
+                true
             }
         }
     }
